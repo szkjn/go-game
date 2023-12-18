417fd950b253f2ebcaf091bb40347e32dd26cded
Jun Suzuki <junsuzuki@gmx.fr>
2023-12-19 00:07:12 +0100

    Added Update() method to Player

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"image"
 	_ "image/png"
+	"math"
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -40,6 +41,7 @@ type Timer struct {
 type Player struct {
 	position Vector
 	sprite   *ebiten.Image
+	rotation float64
 }
 
 func NewPlayer() *Player {
@@ -61,12 +63,28 @@ func NewPlayer() *Player {
 }
 
 func (p *Player) Update() {
+	speed := math.Pi / float64(ebiten.TPS())
 
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		p.rotation -= speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		p.rotation += speed
+	}
 }
 
 func (p *Player) Draw(screen *ebiten.Image) {
+	bounds := p.sprite.Bounds()
+	halfW := float64(bounds.Dx()) / 2
+	halfH := float64(bounds.Dy()) / 2
+
 	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(-halfW, -halfH)
+	op.GeoM.Rotate(p.rotation)
+	op.GeoM.Translate(halfW, halfH)
+
 	op.GeoM.Translate(p.position.X, p.position.Y)
+
 	screen.DrawImage(p.sprite, op)
 }
 
