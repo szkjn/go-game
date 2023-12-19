9f299441fb8a5f7674f158ec833631acf0f5fbe4
Jun Suzuki <junsuzuki@gmx.fr>
2023-12-19 23:02:11 +0100

    Modularized game and player logic and attributes

---
+++ game/game.go
@@ -0,0 +1,103 @@
+package game
+
+import (
+	"fmt"
+	"image/color"
+	"time"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/text"
+)
+
+const (
+	ScreenWidth  = 800
+	ScreenHeight = 600
+
+	meteorSpawnTime = 1 * time.Second
+)
+
+type Game struct {
+	player           *Player
+	meteorSpawnTimer *Timer
+	meteors          []*Meteor
+	bullets          []*Bullet
+	score            int
+}
+
+func NewGame() *Game {
+	g := &Game{
+		meteorSpawnTimer: NewTimer(meteorSpawnTime),
+	}
+
+	g.player = NewPlayer(g)
+
+	return g
+}
+
+func (g *Game) Update() error {
+	g.player.Update()
+
+	g.meteorSpawnTimer.Update()
+	if g.meteorSpawnTimer.IsReady() {
+		g.meteorSpawnTimer.Reset()
+
+		m := NewMeteor()
+		g.meteors = append(g.meteors, m)
+	}
+
+	for _, m := range g.meteors {
+		m.Update()
+	}
+
+	for _, b := range g.bullets {
+		b.Update()
+	}
+
+	for i, m := range g.meteors {
+		for j, b := range g.bullets {
+			if m.Collider().Intersects(b.Collider()) {
+				g.meteors = append(g.meteors[:i], g.meteors[i+1:]...)
+				g.bullets = append(g.bullets[:j], g.bullets[j+1:]...)
+				g.score++
+			}
+		}
+	}
+
+	for _, m := range g.meteors {
+		if m.Collider().Intersects(g.player.Collider()) {
+			g.Reset()
+			break
+		}
+	}
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+
+	for _, m := range g.meteors {
+		m.Draw(screen)
+	}
+
+	for _, b := range g.bullets {
+		b.Draw(screen)
+	}
+
+	g.player.Draw(screen)
+
+	text.Draw(screen, fmt.Sprintf("%06d", g.score), ScoreFont, ScreenWidth/2-100, 50, color.White)
+}
+
+func (g *Game) AddBullet(b *Bullet) {
+	g.bullets = append(g.bullets, b)
+}
+func (g *Game) Reset() {
+	g.player = NewPlayer(g)
+	g.meteors = nil
+	g.bullets = nil
+	g.score = 0
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return ScreenWidth, ScreenHeight
+}
---
+++ game/player.go
@@ -0,0 +1,99 @@
+package game
+
+import (
+	"math"
+	"time"
+
+	"github.com/hajimehoshi/ebiten"
+)
+
+var PlayerSprite = mustLoadImage("assets/player.png")
+
+const (
+	shootCooldown     = time.Millisecond * 100
+	rotationPerSecond = math.Pi
+	bulletSpawnOffset = 50.0
+)
+
+type Player struct {
+	game          *Game
+	position      Vector
+	rotation      float64
+	sprite        *ebiten.Image
+	shootCooldown *Timer
+}
+
+func NewPlayer(game *Game) *Player {
+	sprite := PlayerSprite
+
+	bounds := sprite.Bounds()
+	halfW := float64(bounds.Dx()) / 2
+	halfH := float64(bounds.Dy()) / 2
+
+	pos := Vector{
+		X: ScreenWidth/2 - halfW,
+		Y: ScreenHeight/2 - halfH,
+	}
+
+	return &Player{
+		game:          game,
+		position:      pos,
+		rotation:      0,
+		sprite:        sprite,
+		shootCooldown: NewTimer(shootCooldown),
+	}
+}
+
+func (p *Player) Update() {
+	speed := math.Pi / float64(ebiten.TPS())
+
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		p.rotation -= speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		p.rotation += speed
+	}
+
+	p.shootCooldown.Update()
+	if p.shootCooldown.IsReady() && ebiten.IsKeyPressed(ebiten.KeySpace) {
+		p.shootCooldown.Reset()
+
+		bounds := p.sprite.Bounds()
+		halfW := float64(bounds.Dx()) / 2
+		halfH := float64(bounds.Dy()) / 2
+
+		spawnPos := Vector{
+			p.position.X + halfW + math.Sin(p.rotation)*bulletSpawnOffset,
+			p.position.Y + halfH + math.Cos(p.rotation)*bulletSpawnOffset,
+		}
+
+		bullet := NewBullet(spawnPos, p.rotation)
+		p.game.AddBullet(bullet)
+	}
+}
+
+func (p *Player) Draw(screen *ebiten.Image) {
+	bounds := p.sprite.Bounds()
+	halfW := float64(bounds.Dx()) / 2
+	halfH := float64(bounds.Dy()) / 2
+
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(-halfW, -halfH)
+	op.GeoM.Rotate(p.rotation)
+	op.GeoM.Translate(halfW, halfH)
+
+	op.GeoM.Translate(p.position.X, p.position.Y)
+
+	screen.DrawImage(p.sprite, op)
+}
+
+func (p *Player) Collider() Rect {
+	bounds := p.sprite.Bounds()
+
+	return NewRect(
+		p.position.X,
+		p.position.Y,
+		float64(bounds.Dx()),
+		float64(bounds.Dy()),
+	)
+}
